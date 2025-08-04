aa6f180557420137a5599a9cc289798efbcfad56
Ariel Plaza <arielplazasalinas@gmail.com>
2025-08-04 13:32:20 -0300

    Modulo 3 - Funciones, adaptar funcion

--- curso-2-explorar-funciones-listas/00-proyecto-base/app.js
+++ curso-2-explorar-funciones-listas/00-proyecto-base/app.js
@@ -1,9 +1,10 @@
-let numeroSecreto = generarNumeroSecreto();
-console.log(numeroSecreto);
+let numeroSecreto = 0;
+let intentos = 0;
+
+// console.log(numeroSecreto);
 //seleccion etiqueta desde DOM
 let parrafo = document.querySelector('p');
 //se envia texto desde js 
-parrafo.innerHTML = "Indica un numero del 1 al 10"
 
 function asignarTextoElemento(elemento,texto) {
   let elementoHTML = document.querySelector(elemento);
@@ -12,14 +13,54 @@ function asignarTextoElemento(elemento,texto) {
 }
 
 //funcion
-function intentoDeUsuario(){
-  alert("click desde el boton ")
+function verificarIntento(){
+  let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+  console.log(numeroSecreto);
+  // console.log(numeroDeUsuario);
+  // console.log(numeroDeUsuario == numeroSecreto);
+  if (numeroDeUsuario === numeroSecreto) {
+    asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos == 1 ? 'vez' : 'veces')}`);
+    document.getElementById('reiniciar').removeAttribute("disabled")
+  } else {
+    if (numeroDeUsuario > numeroSecreto) {
+    asignarTextoElemento('p', 'El numero es menor.');
+    } else {
+    asignarTextoElemento('p', 'El numero es mayor.');
+    }
+    intentos++;
+    limpiarCaja();
+  }
   return;
+}
+
+function condicionesIniciales() {
+  asignarTextoElemento('h1', 'Juego del número secreto')
+  asignarTextoElemento('p', 'Indica un numero del 1 al 10')
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+}
+
+
+function limpiarCaja() {
+//  let valorCaja =  document.getElementById('#valorUsuario')
+//   valorCaja.innerHTML = ""
+  document.querySelector('#valorUsuario').value = "";
 }
 
 function generarNumeroSecreto() {
   return Math.floor(Math.random() * 10) + 1;
 }
 
+function reiniciarJuego(params) {
+  //limpiar caja
+  limpiarCaja();
+  //indicar mensaje de inicio
+  condicionesIniciales();
+  //generar numero aleatorio
+  //deshabilitar el boton de nuevo Juego
+  document.querySelector('#reinicia').setAttribute('disabled', 'true');
+  //Inicializar el numero de intentos
+}
+
 //funcion generica
-asignarTextoElemento('h1','Juego del número secreto')
+condicionesIniciales();
---
+++ curso-2-explorar-funciones-listas/03-reiniciando-el-juego/adaptaFuncion.js
@@ -0,0 +1,15 @@
+/*
+El siguiente código en JavaScript calcula el doble y el triple de un número proporcionado. Tu tarea consiste en seleccionar las opciones que sigan las mejores prácticas de programación y ayuden a mejorar la legibilidad y mantenibilidad del código.
+
+function calcularDobleTriple(numero) {
+  const doble = numero * 2;
+  const triple = numero * 3;
+
+  return `El doble de ${numero} es ${doble} y el triple es ${triple}.`;
+}
+
+const numero = 5;
+const resultado = calcularDobleTriple(numero);
+console.log(resultado);
+
+*/
